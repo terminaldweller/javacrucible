b9047281771af59bc9dc5c6380321c3cfc81f1c0
terminaldweller <thabogre@gmail.com>
2022-03-22 02:27:51 +0430

    wip

---
+++ spring-front/public/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const page = (
+  <div>
+    <h1>my awesome website in react</h1>
+    <h3>REasons</h3>
+    <ol>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+      <li>four</li>
+    </ol>
+  </div>
+);
+ReactDOM.render(page, document.getElementById("root"));
---
+++ src/main/java/com/terminaldweller/doc/Md.java
@@ -0,0 +1,21 @@
+package com.terminaldweller.doc;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+/** The markdown class which provides the markdown parser. */
+public class Md {
+  /**
+   * Parses a markdown string and returns the result in html.
+   *
+   * @param markdown the string containing the markdown string.
+   * @return the rendered markdown document in html.
+   */
+  public String mdparseService(String markdown) {
+    Parser parser = parser.builder().build();
+    Node document = parser.parse(markdown);
+    HtmlRenderer renderer = HtmlRenderer.builder().build();
+    return renderer.render(document);
+  }
+}
