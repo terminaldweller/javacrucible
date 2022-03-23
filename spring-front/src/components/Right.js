03ce5a3909a15b1195f36692a1adf5400dc1b760
terminaldweller <thabogre@gmail.com>
2022-03-23 22:05:22 +0430

    wip

--- spring-front/src/components/Right.js
+++ spring-front/src/components/Right.js
@@ -2,5 +2,9 @@ import React from "react";
 import "../index.css";
 
 export default function Right() {
-  return <div class="split right">Parsed Markdown goes here!!!</div>;
+  return (
+    <div class="split right">
+      <p class="markdown-placeholder">Parsed Markdown goes here!!!</p>
+    </div>
+  );
 }
