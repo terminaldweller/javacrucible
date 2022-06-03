7f029b9421ed940f5ca3e7be1e691f5fc2e006a7
terminaldweller <thabogre@gmail.com>
2022-06-03 22:56:37 +0430

    server config

---
+++ src/main/java/com/terminaldweller/main/DevConfiguration.java
@@ -0,0 +1,19 @@
+package com.terminaldweller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedOrigins("https://localhost:7080, https://editor.terminaldweller.com");
+  }
+}
